4058fe2e4914ab84dab3131ace94446966c74240
Vinicius <vinicius@gmail.com>
2018-02-07 10:50:25 -0200

    remoção do request inicial pelo PHP, inclusão de arquivos css e js locais e edição do README.md

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 # list-events-shortcode
 Shortcode for WordPress to list events retrieved using the Mapas Culturais API
 
+# Como usar
+
+Para listar os eventos dentro de uma página ou post utilize o shortcode "list_events" com os atributos:
+
+
+## Atributo URL (*obrigatório*):
+```
+[list_events url=<domínio da API do mapas culturais>]
+```
+O atribuito **URL** recebe como valor o endereço ou domínio onde se encontra a API do mapas culturais.
+
+### Exemplo:
+```
+[list_events url=http://museus.cultura.gov.br ]
+```
+
+## Atributo date_range (*opcional [default=30]*):
+```
+[list_events url=<domínio da API do mapas culturais> date_range=<período de dias>]
+```
+O atributo **date_range** recebe um período de dias para exibir os resultados da pagina inicial
+caso não seja informado o valor padrão é 30 dias
+
+### Exemplo:
+```
+[list_events url=http://museus.cultura.gov.br date_range=20]
+```
 
--- template.php
+++ template.php
@@ -1,6 +1,6 @@
 <div>
 	<h3><?= $atts['title']; ?></h3>
-	<div id="date_listevents" data-url="<?= $url; ?>" class="pull-right" style="">
+	<div id="date_listevents" data-range="<?= $dataRange; ?>" data-url="<?= $url; ?>" class="pull-right" style="">
 	    <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>&nbsp;
 	    <span></span> <b class="caret"></b>
     </div>
@@ -8,23 +8,6 @@
 	<div id="loading" class="spinner" style="display:none;"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>
 
 	<div class="list_spaces">
-		<?php foreach ($events as $event): ?>
-			<div class="list_events_item">
-				<h3><?php echo $event->name; ?></h3>
-				<p><?php echo $event->shortDescription; ?></p>
-
-				<ul>
-					<?php foreach ($event->occurrences as $occurrence): ?>
-						<li style="font-size:0.9em"><i>
-							<strong><?php echo $occurrence->space->name; ?></strong>
-							<?php echo $occurrence->space->endereco;?>
-							<br>
-							<?php echo $occurrence->rule->description, ', ', $occurrence->rule->price; ?>
-							</i>
-						</li>
-					<?php endforeach; ?>
-				</ul>
-			</div>
-		<?php endforeach; ?>
+		
 	</div>
 </div>
